7d73f2063f085a95b1b759ce850807b313017cb8
batibey <58968306+batibey@users.noreply.github.com>
2020-09-13 16:05:11 +0300

    RSS Transactions

---
+++ Rss/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Rss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnStar = new System.Windows.Forms.Button();
+            this.BtnCumhuriyet = new System.Windows.Forms.Button();
+            this.BtnCnn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnStar
+            // 
+            this.BtnStar.BackColor = System.Drawing.Color.Peru;
+            this.BtnStar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnStar.Font = new System.Drawing.Font("OpenSymbol", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStar.Location = new System.Drawing.Point(119, 323);
+            this.BtnStar.Name = "BtnStar";
+            this.BtnStar.Size = new System.Drawing.Size(126, 43);
+            this.BtnStar.TabIndex = 0;
+            this.BtnStar.Text = "Star Gazete";
+            this.BtnStar.UseVisualStyleBackColor = false;
+            this.BtnStar.Click += new System.EventHandler(this.BtnHurriyet_Click);
+            // 
+            // BtnCumhuriyet
+            // 
+            this.BtnCumhuriyet.BackColor = System.Drawing.Color.Peru;
+            this.BtnCumhuriyet.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnCumhuriyet.Font = new System.Drawing.Font("OpenSymbol", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCumhuriyet.Location = new System.Drawing.Point(345, 323);
+            this.BtnCumhuriyet.Name = "BtnCumhuriyet";
+            this.BtnCumhuriyet.Size = new System.Drawing.Size(126, 43);
+            this.BtnCumhuriyet.TabIndex = 1;
+            this.BtnCumhuriyet.Text = "Cumhuriyet";
+            this.BtnCumhuriyet.UseVisualStyleBackColor = false;
+            this.BtnCumhuriyet.Click += new System.EventHandler(this.BtnCumhuriyet_Click);
+            // 
+            // BtnCnn
+            // 
+            this.BtnCnn.BackColor = System.Drawing.Color.Peru;
+            this.BtnCnn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnCnn.Font = new System.Drawing.Font("OpenSymbol", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCnn.Location = new System.Drawing.Point(574, 323);
+            this.BtnCnn.Name = "BtnCnn";
+            this.BtnCnn.Size = new System.Drawing.Size(126, 43);
+            this.BtnCnn.TabIndex = 2;
+            this.BtnCnn.Text = "CNN TÜRK";
+            this.BtnCnn.UseVisualStyleBackColor = false;
+            this.BtnCnn.Click += new System.EventHandler(this.BtnCnn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(119, 283);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(126, 25);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(345, 283);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(126, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(574, 283);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(126, 25);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(27, 42);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(771, 224);
+            this.listBox1.TabIndex = 6;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(795, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(32, 27);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(827, 391);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnCnn);
+            this.Controls.Add(this.BtnCumhuriyet);
+            this.Controls.Add(this.BtnStar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStar;
+        private System.Windows.Forms.Button BtnCumhuriyet;
+        private System.Windows.Forms.Button BtnCnn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Rss/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+
+namespace Rss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+
+
+        private void BtnHurriyet_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            XmlTextReader xmloku = new XmlTextReader("http://www.stargazete.com/rss/rss.asp");
+            while (xmloku.Read())
+            {
+                if(xmloku.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku.ReadString());
+                }
+            }
+        }
+
+        private void BtnCumhuriyet_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            XmlTextReader xmloku = new XmlTextReader("http://www.cumhuriyet.com.tr/rss/son_dakika.xml");
+            while (xmloku.Read())
+            {
+                if (xmloku.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku.ReadString());
+                }
+            }
+
+        }
+
+        private void BtnCnn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            XmlTextReader xmloku = new XmlTextReader("http://www.cnnturk.com/feed/rss/news");
+            while (xmloku.Read())
+            {
+                if (xmloku.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku.ReadString());
+                }
+            }
+        }
+    }
+}
